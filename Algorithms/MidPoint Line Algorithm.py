4ef96bf7d59d682ffe9300c848ba5a12f2e05b0b
utp <utp@utp>
2016-04-18 12:21:02 -0500

    Create function MidPoint Line Algorithm

--- Algorithms/MidPoint Line Algorithm.py
+++ Algorithms/MidPoint Line Algorithm.py
@@ -27,33 +27,31 @@ def draw_origin(sc, color, border):
     pygame.draw.line(sc, color, [xcar(-width/2), ycar(0)], [xcar(width/2), ycar(0)], border)
     pygame.draw.line(sc, color, [xcar(0), ycar(-height/2)], [xcar(0), ycar(height/2)], border)
 
-'''
 # MidPoint line algorithm
-def drawLine(p0, p1):
+def drawLine(p0, p1, color):
     dy = p1[1] - p0[1]
     dx = p1[0] - p0[0]
     d = 2 * dy - dx
 
     x = p0[0]
     y = p0[1]
-    while (x < p1[0]):
-        screen.set_at((x, y), blue)
+    while (x <= p1[0]):
+        screen.set_at((xcar(x), ycar(y)), color)
         print (x, y)
         if d > 0:
             y = y + 1
             d = d - (2 * dx)
         x = x + 1
         d = d + (2 * dy)
-'''
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode([width, height])
 
-    #pygame.draw.rect(screen, white, [0, 0, 800, 800])
-
     draw_origin(screen, white, 2)
-    #drawLine([0, 400], [800, 600])
+    drawLine([-400, -10], [400, 10], blue)
+    drawLine([-400, -50], [400, 50], green)
+    drawLine([-400, -200], [400, 200], red)
 
     pygame.display.flip()
 
